cee8ffbf009b589f3645cc27f4fcc46bc9f6fa5f
Trikke <tristan@netlog.com>
2016-10-17 16:00:50 +0200

    change package and add features i wanted

--- library/src/main/java/be/trikke/swipestack/util/AnimationUtils.java
+++ library/src/main/java/be/trikke/swipestack/util/AnimationUtils.java
@@ -14,26 +14,23 @@
  * limitations under the License.
  */
 
-package link.fls.swipestack.util;
+package be.trikke.swipestack.util;
 
 import android.animation.Animator;
 
 public class AnimationUtils {
 
-    public static abstract class AnimationEndListener implements Animator.AnimatorListener {
-        @Override
-        public void onAnimationStart(Animator animation) {
-            // Do nothing
-        }
+	public static abstract class AnimationEndListener implements Animator.AnimatorListener {
+		@Override public void onAnimationStart(Animator animation) {
+			// Do nothing
+		}
 
-        @Override
-        public void onAnimationCancel(Animator animation) {
-            // Do nothing
-        }
+		@Override public void onAnimationCancel(Animator animation) {
+			// Do nothing
+		}
 
-        @Override
-        public void onAnimationRepeat(Animator animation) {
-            // Do nothing
-        }
-    }
+		@Override public void onAnimationRepeat(Animator animation) {
+			// Do nothing
+		}
+	}
 }
